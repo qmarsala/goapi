67a885e269a174200bb265a22c182b62d3b598d3
Quinten Marsala <quinten.marsala@gmail.com>
2024-04-20 09:24:33 -0700

    get by id and update

--- posts.go
+++ posts.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -14,9 +15,38 @@ type post struct {
 
 func getPosts(db *gorm.DB, c *gin.Context) {
 	posts := []post{}
-	db.Limit(10).Find(&posts)
-	fmt.Println(posts)
+	db.Limit(25).Find(&posts)
 	c.JSON(200, posts)
+}
+
+func getPostById(db *gorm.DB, id uint) (*post, error) {
+	p := &post{}
+	tx := db.Find(&p, "ID = ?", uint(id))
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	if tx.RowsAffected < 1 {
+		return nil, nil
+	}
+	return p, nil
+}
+
+func getPost(db *gorm.DB, c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	p, err := getPostById(db, uint(id))
+	switch {
+	case err != nil:
+		c.Status(500)
+	case p != nil:
+		c.JSON(200, p)
+	default:
+		c.Status(404)
+	}
 }
 
 func createPost(db *gorm.DB, c *gin.Context) {
@@ -25,8 +55,30 @@ func createPost(db *gorm.DB, c *gin.Context) {
 }
 
 func updatePost(db *gorm.DB, c *gin.Context) {
-	p := &post{Message: "test"}
-	c.JSON(200, p)
+	fmt.Println(c.Request)
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	p, err := getPostById(db, uint(id))
+	if p != nil {
+		update := &post{}
+		c.Bind(update)
+		//todo: validate request
+		tx := db.Model(p).UpdateColumns(update)
+		if tx.Error == nil {
+			c.JSON(200, p)
+		} else {
+			c.Status(500)
+		}
+	}
+	if err != nil {
+		c.Status(500)
+		return
+	}
+	c.Status(404)
 }
 
 func deletePost(db *gorm.DB, c *gin.Context) {
