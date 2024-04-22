ced3b954d73725ff7783508a3f3c5f878076dad0
Quinten Marsala <quinten.marsala@gmail.com>
2024-04-22 14:35:20 -0700

    add spa to project

--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -13,7 +14,7 @@ import (
 func main() {
 	db := initializeDB()
 	api := gin.Default()
-
+	api.Use(cors.Default())
 	api.GET("/api/posts", makeHandler(db, getPosts))
 	api.POST("/api/posts", makeHandler(db, createPost))
 	api.GET("/api/posts/:id", makeHandler(db, getPost))
---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/posts')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+    </div>
+  );
+}
+
+export default App;
